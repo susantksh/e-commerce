fca06e6cff0f4ec40759594623714c4d909333a3
susantksh <susantbm683@gmail.com>
2022-08-14 14:25:44 -0400

    code added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -119,11 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = BASE_DIR 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-STATIC_HOST = "https://d4663kmspf1sqa.cloudfront.net" if not DEBUG else ""
-STATIC_URL = STATIC_HOST + "/static/"
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
